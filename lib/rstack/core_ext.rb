e8e27b859a33d3e3154e4db1a7d4c0acaa6d0dc9
Jeremy Burks <jeremy.burks@gmail.com>
2008-04-05 10:47:46 -0500

    renamed pathize to core_ext

---
+++ lib/rstack/core_ext.rb
@@ -0,0 +1,13 @@
+module RStack #:nodoc:
+  module CoreExtensions #:nodoc:
+    module String
+      def pathize
+        self.gsub(/-/, '/')
+      end
+    end
+  end
+end
+
+class String
+  include RStack::CoreExtensions::String
+end

b7659263cea70998710a7afc9c5484d738efc30a
Jeremy Burks <jeremy.burks@gmail.com>
2009-05-02 17:59:41 -0500

    got rid of the null object stuff in the spec helper, its  deprecated and it was worthless anyway (i always removed it)

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -1,14 +1,6 @@
 require '#{main.chomp('.rb')}'
 
-module SpecHelper
-  def null_mock(name, options = {})
-    mock name, options.merge(:null_object => true)
-  end
-end
-
 Spec::Runner.configure do |config|
-  config.include(SpecHelper)
-  
   # config.before(:each) do
   # end
 end
